76f099e1d174fbb9237b2117365d9c0e00b86fa8
Yaroslav Balan <baryarus@gmail.com>
2026-05-25 23:50:23 +0300

    fix: add env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: process.env.CORS_ORIGIN?.split(',') ?? 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
